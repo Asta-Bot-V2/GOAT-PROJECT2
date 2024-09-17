63910f28472c540625d7e31c9a41d05ef07dca5c
Asta ichiyukimori <162071948+Asta-Bot-V2@users.noreply.github.com>
2024-09-17 03:21:01 +0100

    Update 00000help.js

--- scripts/cmds/00000help.js
+++ scripts/cmds/00000help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🇵🇭|𝗞𝗬𝗟𝗘'𝗦 𝗚𝗢𝗔𝗧𝗕𝗢𝗧𝗩𝟮";
+const doNotDelete = "𝐀𝐒𝐓𝐀 𝐁𝐎𝐓";
 /** 
 * @author NTKhang
 * @author: do not delete it
